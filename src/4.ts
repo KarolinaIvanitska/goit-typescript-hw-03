a03b5142fedac470b4208ecb57888de2ddf158d4
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-04-28 22:04:52 +0200

    4-task

--- src/4.ts
+++ src/4.ts
@@ -1,5 +1,66 @@
-class key {
-  private signature;
+//клас Key
+
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+//клас Person
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+// Абстрактний клас будинку
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person entered the house.");
+    } else {
+      console.log("Cannot enter. The door is closed.");
+    }
+  }
+}
+
+// Клас мого будинку
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door opened.");
+    } else {
+      console.log("Cannot open the door. Wrong key.");
+    }
+  }
 }
 
 const key = new Key();
